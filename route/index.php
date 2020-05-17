b12b401428fa534ca5e57ba00065e570761877c3
MenaraWas <54022002+MenaraWas@users.noreply.github.com>
2020-05-17 12:12:48 +0700

    task Routing

---
+++ route/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Menara Lintang Was menaralintang.2019 e4eea37f</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Autos Database</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="view.php">view.php</a> without logging in - it should fail with an error message.
+</p>
+<p>
+Attempt to go to 
+<a href="add.php">add.php</a> without logging in - it should fail with an error message.
+</p>
+<p>
+</div>
+</body>
+</html>
+
+
+
